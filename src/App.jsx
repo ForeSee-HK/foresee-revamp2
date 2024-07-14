7c41a5921d854f046d50d89c601d7394187ac89b
IsaacIndex <isaacwongnh@gmail.com>
2024-07-14 19:58:29 +0800

    Game page layout and fix margin on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<About />} />
-        {/* <Route path="/about-foresee" element={<About />} /> */}
-        <Route path="/join-foresee" element={<Join />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/educational-games" element={<Games />} />
-      </Routes>
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<About />} />
+          {/* <Route path="/about-foresee" element={<About />} /> */}
+          <Route path="/join-foresee" element={<Join />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/educational-games" element={<Games />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/pages/Games.jsx
+++ src/components/pages/Games.jsx
@@ -4,8 +4,14 @@ import SnakeGameImage from "../../assets/Games/SnakeGame.png"
 export const Games = () => {
   return (
     <>
-      <p className="game-title">Explore Our Educational Games</p>
-      <img src={SnakeGameImage} alt="" />
+      <p className="games-heading">Explore Our Educational Games</p>
+      <div className="game-container">
+        <img className="game-image" src={SnakeGameImage} alt="" />
+        <div>
+          <p className="game-heading">Colourblindness Snake Game</p>
+          <p className="game-paragraph">In this game, you control the snake with either keyboard or virtual joystick. When an apple is eaten, screen will switch to a colourblindness.</p>
+        </div>
+      </div>
     </>
   )
 };
