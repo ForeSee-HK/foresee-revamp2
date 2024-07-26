a2733ce11be21a0be0d8a83a928bf28b20a9578d
IsaacIndex <isaacwongnh@gmail.com>
2024-07-26 23:48:08 +0900

    test import git link

--- src/components/pages/Games.jsx
+++ src/components/pages/Games.jsx
@@ -1,5 +1,7 @@
 import "./Games.css"
 import SnakeGameImage from "../../assets/games-page/SnakeGame.png"
+// import Test from "@isaacindex/npm-test"
+import Test from "@isaacindex/snake-on-page"
 
 export const Games = () => {
   return (
@@ -11,6 +13,7 @@ export const Games = () => {
           <p className="game-heading">Colourblindness Snake Game</p>
           <p className="game-paragraph">In this game, you control the snake with either keyboard or virtual joystick. When an apple is eaten, screen will switch to a colourblindness.</p>
         </div>
+        <Test />
       </div>
     </>
   )
