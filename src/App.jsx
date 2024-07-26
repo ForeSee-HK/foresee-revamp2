1c7de8802a553af9cca11dbcc3bac7abbc17ec8f
IsaacIndex <isaacwongnh@gmail.com>
2024-07-27 03:58:26 +0900

    layouts for games (no navbar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import "./App.css";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 import { Navbar } from "./components/Navbar";
 import { About, Events, Games, Home, Join } from "./components/pages";
 
-function App() {
+// Games
+import SnakeOnPage from "@isaacindex/snake-on-page"
+
+// Main layout component with navbar
+const MainLayout = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <div className="page-content">
-        <Routes>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+// Layout component without navbar
+const NoNavbarLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
           <Route path="/" element={<About />} />
           {/* <Route path="/about-foresee" element={<About />} /> */}
           <Route path="/join-foresee" element={<Join />} />
           <Route path="/events" element={<Events />} />
           <Route path="/educational-games" element={<Games />} />
-        </Routes>
-      </div>
-    </div>
+        </Route>
+        <Route element={<NoNavbarLayout />}>
+          {/* Game Routes */}
+          <Route path="/snake-on-page" element={<SnakeOnPage />} />
+        </Route>
+      </Routes >
+    </>
   );
 }
 
--- src/components/pages/Games.jsx
+++ src/components/pages/Games.jsx
@@ -4,17 +4,22 @@ import SnakeGameImage from "../../assets/games-page/SnakeGame.png"
 import Test from "@isaacindex/snake-on-page"
 
 export const Games = () => {
+  const handleGameClick = (path) => {
+    window.open(path, '_blank');
+  }
+
   return (
     <>
       <p className="games-heading">Explore Our Educational Games</p>
-      <div className="game-container">
+      {/* Snake Game */}
+      <div className="game-container" onClick={() => handleGameClick("snake-on-page")}>
         <img className="game-image" src={SnakeGameImage} alt="" />
         <div>
           <p className="game-heading">Colourblindness Snake Game</p>
           <p className="game-paragraph">In this game, you control the snake with either keyboard or virtual joystick. When an apple is eaten, screen will switch to a colourblindness.</p>
         </div>
       </div>
-      <Test />
+
     </>
   )
 };
