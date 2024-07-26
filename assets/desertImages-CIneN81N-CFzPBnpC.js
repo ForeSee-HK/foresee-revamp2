7cd61f03580ac7963cbc84f02ae497d09269c053
IsaacIndex <isaacwongnh@gmail.com>
2024-07-27 05:23:22 +0900

    Updates

---
+++ assets/desertImages-CIneN81N-CFzPBnpC.js
@@ -0,0 +1,1 @@
+const a="/desert_map/map.png",p="/desert_map/map_achromatopsia.png",t="/desert_map/map_deuteranopia.png",e="/desert_map/map_protanopia.png",o="/desert_map/map_tritanopia.png",r={normal:a,achromatopsia:p,deuteranopia:t,protanopia:e,tritanopia:o};export{r as default};
