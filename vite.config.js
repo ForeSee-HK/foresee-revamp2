44a5af3d1f3ce34590f1c6b68dfb56e62893ba63
IsaacIndex <isaacwongnh@gmail.com>
2024-08-01 08:56:32 +0900

    minified chunks with manualChunks

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/foresee-revamp2/",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
