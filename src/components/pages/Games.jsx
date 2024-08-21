b4e3980e1ce82150ef5ae9b0dd29f0bd51fc79fa
IsaacIndex <isaacwongnh@gmail.com>
2024-08-22 02:33:08 +0800

    successful import using 'public' images

--- src/components/pages/Games.jsx
+++ src/components/pages/Games.jsx
@@ -14,7 +14,7 @@ export const Games = () => {
       <p className="games-heading">Explore Our Educational Games</p>
       {/* Snake Game */}
       <div className="game-container">
-        <img className="game-image" src={SnakeGameImage} alt="" />
+        <img className="game-image" src={SnakeGameImage} alt="Snake Game Image" />
         <div>
           <p className="game-heading">Colourblindness Snake Game</p>
           <p className="game-paragraph">In this game, you control the snake with either keyboard or virtual joystick. When an apple is eaten, screen will switch to a colourblindness.</p>
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/foresee-revamp2/",
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
+  plugins: [react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'node_modules/**/maps/*',
+        dest: 'maps'
       }
-    }
-  }
+    ]
+    // targets: [
+    //   {
+    //     src: 'node_modules/**/public/*',
+    //     dest: ''
+    //   }
+    // ]
+  })
+  ],
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 })
