69ba58d553cddf252588222519432bbcc2846a8a
IsaacIndex <69638683+IsaacIndex@users.noreply.github.com>
2025-03-11 09:15:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://foresee-revamp2.pages.dev/",
+  base: "/",
   plugins: [react(),
   viteStaticCopy({
     targets: [
