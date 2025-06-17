e93e71c3ce14d2705d549ebc664105dcfc70db53
이준모 <lijun@ijunmos-MacBook-Air.local>
2025-06-18 01:57:47 +0900

    Add: initial layout for Events page

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -1,6 +1,8 @@
 import "./Events.css";
 import PastEvents from "./PastEvents";
 import CollabOpportunities from "../CollabOpportunities";
+import UpcomingEvents from "./UpcomingEvents";
+import { Link } from "react-router-dom";
 
 export const Events = () => {
   return (
@@ -8,10 +10,11 @@ export const Events = () => {
 
     // </div>
     <div>
-      <div className="upcoming-events-container">
-        <p className="subsection-heading">Upcoming Events</p>
+      
+        {/* <p className="subsection-heading">Upcoming Events</p> */}
+        <UpcomingEvents/>
         <p className="">There are no upcoming events at the moment. Please wait</p>
-      </div>
+
       <div className="past-events-section">
         <p className="subsection-heading">Past Events</p>
         <PastEvents />
